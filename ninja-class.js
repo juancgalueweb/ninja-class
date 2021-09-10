13925fb67eda405bd85fb1eff5d5d199823ce924
juancgalueweb <juancgalue@icloud.com>
2021-09-09 23:51:35 -0300

    Sensei homework

--- ninja-class.js
+++ ninja-class.js
@@ -8,18 +8,18 @@
 // Agrega un método: drinkSake() - debe agregar +10 a la salud del Ninja
 
 class Ninja {
-  constructor(nombre, salud, velocidad = 3, fuerza = 3) {
+  constructor(nombre) {
     this.nombre = nombre;
-    this.salud = salud;
-    this.velocidad = velocidad;
-    this.fuerza = fuerza;
+    this.salud = 50;
+    this.velocidad = 3;
+    this.fuerza = 3;
   }
   sayName() {
     console.log(`Mi nombre es ${this.nombre}`);
   }
   showStats() {
     console.log(
-      `Nombre del Ninja - ${this.nombre}, fuerza +${this.fuerza}, velocidad +${this.velocidad} y salud +${this.salud}`
+      `Nombre: ${this.nombre}, Salud: ${this.salud}, Velocidad: ${this.velocidad}, Fuerza: ${this.fuerza}`
     );
   }
   drinkSake() {
@@ -35,8 +35,33 @@ ninja1.drinkSake();
 ninja1.drinkSake();
 ninja1.showStats();
 
-const ninja2 = new Ninja("Jinichi Kawakami", 95, 9, 15);
+const ninja2 = new Ninja("Jinichi Kawakami", 95);
 ninja2.sayName();
 ninja2.showStats();
 ninja2.drinkSake();
 ninja2.showStats();
+
+// Extiende la clase Ninja y crea la clase Sensei. Un Sensei debería tener 200 de salud, 10 de velocidad y 10 de fuerza por defecto.
+// Además, un Sensei debería tener un nuevo atributo llamado sabiduría , y el valor predeterminado debería ser 10.
+// Finalmente, agrega el método speakWisdom()
+// speakWisdom()debería llamar al método drinkSake() desde la clase Ninja, antes de console.logging, un mensaje inteligente.
+
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name);
+    this.salud = 200;
+    this.velocidad = 10;
+    this.fuerza = 10;
+    this.sabiduria = 10;
+  }
+  speakWiswom() {
+    super.drinkSake();
+    console.log(
+      "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+    );
+  }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWiswom();
+superSensei.showStats();
