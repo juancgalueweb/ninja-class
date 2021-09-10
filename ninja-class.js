653fd9e8fbee5ffd4dd89c4f64d88f11cdf22767
juancgalueweb <juancgalue@icloud.com>
2021-09-09 22:42:05 -0300

    first commit, ninja-class

---
+++ ninja-class.js
@@ -0,0 +1,36 @@
+// Crea una clase ninja
+// Agregar un atributo: nombre
+// Agregar un atributo: salud
+// Agregar un atributo: velocidad, da un valor predeterminado de 3
+// Agregar un atributo: fuerza, valor predeterminado de 3
+// Agregar un método: sayName() - Esto debería registrar el nombre del ninja en la cónsola
+// Agregar un método: showStats() - Debe mostrar el nombre, fuerza, velocidad y la salud del Ninja
+// Agrega un método: drinkSake() - debe agregar +10 a la salud del Ninja
+
+class Ninja {
+  constructor(nombre, salud, velocidad = 3, fuerza = 3) {
+    this.nombre = nombre;
+    this.salud = salud;
+    this.velocidad = velocidad;
+    this.fuerza = fuerza;
+  }
+  sayName() {
+    console.log(`Mi nombre es ${this.nombre}`);
+  }
+  showStats() {
+    console.log(
+      `Nombre del Ninja - ${this.nombre}, fuerza - ${this.fuerza}, velocidad - ${this.velocidad} y salud +${this.salud}`
+    );
+  }
+  drinkSake() {
+    this.salud += 10;
+  }
+}
+
+const ninja1 = new Ninja("Fujibayashi Nagato", 80, 15, 20);
+
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.drinkSake();
+ninja1.showStats();
