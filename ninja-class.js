4639c930b4b74b428448688aaf4d1990a923c547
juancgalueweb <juancgalue@icloud.com>
2021-09-09 22:52:19 -0300

    another ninja added for testing

--- ninja-class.js
+++ ninja-class.js
@@ -19,7 +19,7 @@ class Ninja {
   }
   showStats() {
     console.log(
-      `Nombre del Ninja - ${this.nombre}, fuerza - ${this.fuerza}, velocidad - ${this.velocidad} y salud +${this.salud}`
+      `Nombre del Ninja - ${this.nombre}, fuerza +${this.fuerza}, velocidad +${this.velocidad} y salud +${this.salud}`
     );
   }
   drinkSake() {
@@ -27,10 +27,16 @@ class Ninja {
   }
 }
 
-const ninja1 = new Ninja("Fujibayashi Nagato", 80, 15, 20);
+const ninja1 = new Ninja("Fujibayashi Nagato", 80);
 
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.drinkSake();
 ninja1.showStats();
+
+const ninja2 = new Ninja("Jinichi Kawakami", 95, 9, 15);
+ninja2.sayName();
+ninja2.showStats();
+ninja2.drinkSake();
+ninja2.showStats();
